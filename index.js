f80ddbdd49f4a58a9ba8bed61215e21b47938e8c
ronemsb-cpu <rone.msb@gmail.com>
2025-12-14 19:32:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+const qrcode = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
+
+const client = new Client({
+    puppeteer: {
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('WhatsApp conectado com sucesso!');
+});
+
+client.on('message', msg => {
+    if (
+        msg.body &&
+        /(oi|ola|olá|informação|informacoes|quero|comprar|site|link)/i.test(msg.body)
+    ) {
+        client.sendMessage(
+            msg.from,
+            'Olá 👋\n\n' +
+            'Aqui é o *Macson*, representante oficial do *Trans Resveratrol Americano*.\n\n' +
+            'Acesse o site oficial:\n\n' +
+            'https://rebrand.ly/Trans-ReveraPromo'
+        );
+    }
+});
+
+client.initialize();
 
